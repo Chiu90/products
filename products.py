1df1c6109ca096940842e476631c621c021aafee
Chiu Ling <corinna930512@gmail.com>
2022-05-25 18:35:32 +0800

    products

---
+++ products.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name =='q':
+		break
+	price= input('請輸入商品價格:')	
+	p = []
+	p.append(name)
+	p.append(price)#上面三行等於p = [name,price]
+	products.append(p)
+print(products)
+
+products[0][0]#等於products裡的地0格的第0格[(n,p),(n,p)]等於第一個小括號裡的第一個
